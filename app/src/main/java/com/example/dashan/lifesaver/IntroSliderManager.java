65a029f48f678d2a2c86c564f85f67f8948e99f6
dashan124 <dashanjotsingh961999@gmail.com>
2018-05-31 10:07:17 +0530

    IntroSlider Added

---
+++ app/src/main/java/com/example/dashan/lifesaver/IntroSliderManager.java
@@ -0,0 +1,29 @@
+package com.example.dashan.lifesaver;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by dashan on 31/5/18.
+ */
+
+public class IntroSliderManager {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    Context mContext;
+
+    public IntroSliderManager(Context context){
+        this.mContext=context;
+        sharedPreferences=context.getSharedPreferences("first",0);
+        editor=sharedPreferences.edit();
+    }
+    public void setFirst(boolean isFirst){
+        editor.putBoolean("check",isFirst);
+        editor.commit();
+    }
+    public boolean Check(){
+        return  sharedPreferences.getBoolean("check",true);
+    }
+
+}
+
